edb31b601e5f6b734e07212de580dadeb89d36cb
kidp330 <j.migdal42@gmail.com>
2024-05-01 18:42:35 +0200

    Modify run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,34 @@
 set -eux
 
-root_dir=$(dirname $BASH_SOURCE)
+if [[ ! -v IMAGE_PATH ]]; then 
+  echo '$IMAGE_PATH not provided, exiting...'
+  exit 1
+fi
+
+if [[ ! -v REPOS ]]; then
+  REPOS=$HOME/repos
+fi
+
+if [[ $- =~ i ]]; then
+  root_dir=$PWD
+else
+  root_dir=$(dirname $BASH_SOURCE)
+fi
+
 source $root_dir/conf.env
 source $root_dir/common.sh
 
-image_dir=$(_strip_path $root_dir $1)
+image_dir=$(_strip_path $root_dir $IMAGE_PATH)
 
-if [[ -f "$1/run.sh" ]]; then
-  source $1/run.sh
+if [[ -f "$IMAGE_PATH/run.sh" ]]; then
+  source $IMAGE_PATH/run.sh
 else
-  # TODO: make pull policy configurable
+  # TODO: make replace policy configurable
   docker run -dt \
     --name=$image_dir \
-    --pull=never \
-    --volume=$HOME/repos:$WORKSPACE \
+    --pull=$PULL_POLICY \
+    --replace \
+    --volume=$REPOS:$WORKSPACE \
+    --volume=$DOTFILES:/root \
     localhost/$image_dir:dev
 fi
